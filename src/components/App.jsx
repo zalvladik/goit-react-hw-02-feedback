aed7af2172be4338a5242937565fac88e5441437
zalvladik <zalipkav@ukr.net>
2023-04-14 17:07:11 +0300

    53443kk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import FeedbackContainer from './Counter/Counter'
+import React from 'react'
+
+class App extends React.Component{
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
+
+  handleIncrement = (event) => {
+    console.log(event.currentTarget.textContent.toLowerCase());
+    const value = event.currentTarget.textContent.toLowerCase();
+  this.setState(prevState => ({
+    [value]:prevState[value] + 1,
+  }))
+  this.countTotalFeedback(event)
+  }
+  countTotalFeedback = () =>{
+    const total = this.state.good+this.state.neutral+this.state.bad;
+    return total;
+}
+
+  countPositiveFeedbackPercentage = () =>{
+    const total = this.state.good+this.state.neutral+this.state.bad;
+    const result = this.state.good/total*100;
+    return Math.round(result)+'%';
+} 
+  render(){
+    return (
+    <FeedbackContainer 
+    handleIncrement={this.handleIncrement}
+    good={this.state.good} 
+    neutral={this.state.neutral}
+    bad={this.state.bad}
+    total={this.countTotalFeedback()}
+    percentage={this.countPositiveFeedbackPercentage()}
+    />
   );
+}
 };
+
+export default App
---
+++ src/components/Counter/CounterStyled.jsx
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+margin: 30px 0px 0px 30px;
+`
+
+export const ButtonUl = styled.ul`
+display:flex;
+`
+export const Button = styled.button`
+margin-right:10px;
+font-size:20px;
+border-radius:4px;
+border:1px solid black;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+cursor:pointer;
+&:hover{
+    background-color:#0b9ca3;
+}
+`
+
+export const StatisticsUl = styled.ul`
+list-style:none;
+`
+
+export const Span = styled.span`
+margin-left:20px;
+font-size:20px;
+font-weight: 500;
+`
+
+export const StatisticLi = styled.li`
+font-size:20px;
+margin-bottom:10px;
+`
